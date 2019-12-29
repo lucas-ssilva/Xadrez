a6a4b54c44356e916059cd2c80c9cbc8cb160539
Lucas <lucas_ssilva@outlook.com>
2019-12-29 00:06:38 -0300

    criação da primeira classe

--- Xadrez-Console/Tabuleiro/Posicao.cs
+++ Xadrez-Console/Tabuleiro/Posicao.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Xadrez_Console.Tabuleiro
+﻿namespace Tabuleiro
 {
     class Posicao
     {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao() { }
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
     }
 }
